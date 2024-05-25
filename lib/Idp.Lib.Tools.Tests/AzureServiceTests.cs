ee3ca62c3967f7b2e8be675aff7e4905f27fb65a
Ian Philpot <ianphil@microsoft.com>
2024-05-25 10:09:29 -0400

    Project init

---
+++ lib/Idp.Lib.Tools.Tests/AzureServiceTests.cs
@@ -0,0 +1,10 @@
+namespace Idp.Lib.Tools.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ lib/Idp.Lib.Tools/AzureService.cs
@@ -0,0 +1,31 @@
+﻿namespace Idp.Lib.Tools;
+
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+
+public class AzureService
+{
+    private readonly string _subscriptionId;
+    private readonly DefaultAzureCredential _credential;
+    private readonly ArmClient _armClient;
+
+    public AzureService(string subscriptionId)
+    {
+        _subscriptionId = subscriptionId;
+        _credential = new DefaultAzureCredential();
+        _armClient = new ArmClient(_credential, _subscriptionId);
+    }
+
+    public ResourceGroupResource GetResourceGroup(string resourceGroupName)
+    {
+        var resourceId = MakeResourceGroupResourceIdentifier(resourceGroupName);
+        return _armClient.GetResourceGroupResource(resourceId);
+    }
+
+    public ResourceIdentifier MakeResourceGroupResourceIdentifier(string resourceGroupName)
+    {
+        return new ResourceIdentifier($"/subscriptions/{_subscriptionId}/resourceGroups/{resourceGroupName}");
+    }
+}
