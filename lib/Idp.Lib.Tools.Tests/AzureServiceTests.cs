87d30f783ba9fd8e44d377ace3ed6645ff902a9f
Ian Philpot <ianphil@microsoft.com>
2024-05-25 11:19:38 -0400

    adds pr workflow

--- lib/Idp.Lib.Tools.Tests/AzureServiceTests.cs
+++ lib/Idp.Lib.Tools.Tests/AzureServiceTests.cs
@@ -1,10 +1,17 @@
 namespace Idp.Lib.Tools.Tests;
 
-public class UnitTest1
+using Idp.Lib.Tools;
+
+public class AzureServiceTests
 {
     [Fact]
-    public void Test1()
+    public void Test_MakeResourceGroupResourceIdentifier()
     {
+        var azSvc = new AzureService("subId");
 
+        var expected = $"/subscriptions/subId/resourceGroups/rg";
+        var actual = azSvc.MakeResourceGroupResourceIdentifier("rg");
+
+        Assert.Equal(expected, actual);
     }
 }
